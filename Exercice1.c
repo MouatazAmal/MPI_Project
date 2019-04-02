5b8f2e1be9b5d924f9bb33359111aa4b287933fb
Mouataz <mouataz.amal@gmail.com>
2019-04-02 19:43:36 +0200

    Sa mére j'ai réussi à broadcast la diagonal

--- Exercice1.c
+++ Exercice1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <mpi.h>
-#define N 10
+#define N 3
 #define ROOT 0
 
 // Compilation : mpicc -o Exercice1 Exercice1.c
@@ -12,75 +12,97 @@
 
 int main(int argc, char *argv[]){
 		
-	int my_rank,col,row;
+	int my_rank;
 	MPI_Status status;
-	MPI_Comm grid_comm,cc,cl;
-	int gridRank;
+	MPI_Comm grid_comm,row_comm,line_comm;
+	int gridRank, rRank, lRank;
 	int coordinates[2];
 	int period[2] = {1,1};
 
-	int A[N][N];
-	int B[N][N];
-	int C[N][N];
-
 	MPI_Init(&argc, &argv); // Initialise l'env d'exécution
+	// Creating ranks of threads.
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
 	int nbProcess;
+	// affecting to nbProcess the number fo process
 	MPI_Comm_size(MPI_COMM_WORLD,&nbProcess);
-	int Dims[2] = {0,0};
+	int Dims[2] = {3,3};
 	MPI_Dims_create(nbProcess,2,Dims);
  	MPI_Cart_create(MPI_COMM_WORLD, 2, Dims, period, 1,&grid_comm);
 
  	MPI_Comm_rank(grid_comm,&gridRank);
-    MPI_Cart_coords(grid_comm, gridRank, 2, coordinates);
-    row = coordinates[0];
-    col = coordinates[1];
 
-    int remain[2] = {1,0};
-	MPI_Cart_sub(grid_comm,remain, &cl);
-	remain[0] = 0;
-	remain[1] = 1;
-	MPI_Cart_sub(grid_comm,remain, &cc);
-	if (my_rank == ROOT) printf("The created Grid is ( %d , %d ) size grid \n",Dims[0],Dims[1]);
-	MPI_Barrier(MPI_COMM_WORLD);
-	int id1,id2;
+	MPI_Cart_coords(grid_comm, gridRank, 2, coordinates);
+	int row = coordinates[0];
+	int col = coordinates[1];
+
+	int row_def[2] = {1,0};
+	MPI_Cart_sub(grid_comm,row_def, &row_comm);
+	int col_def[2] = {0,1};
+	MPI_Cart_sub(grid_comm,col_def, &line_comm);
 	
-    int coord[] = {3,1};
-    MPI_Cart_rank(grid_comm, coord, &id1);
-    coord[0] = 3;
-    coord[1] = 2;
-    MPI_Cart_rank(grid_comm, coord, &id2);
+	MPI_Comm_rank(row_comm,&rRank);
+	MPI_Comm_rank(line_comm,&lRank);
 
-    int buff = -1;
-    if (my_rank == ROOT)
-    {
-    	buff = 42;
-    }
-    MPI_Bcast(&buff, 1, MPI_INT, ROOT, cl);
+	if (gridRank == 0) printf("The created Grid is ( %d , %d ) size grid \n",Dims[0],Dims[1]);
 
-    if (my_rank != ROOT) {
-	    printf("Rank %d receives %d from process %d\n", my_rank, buff, ROOT);
-	}
-    
-    /*
-    if (my_rank == id1)
-    {
-    	int number = 42;
-    	MPI_Send(&number, 1, MPI_INT, id2,99, cl);
-    	printf("rank 31 send %d\n",number );
-    	MPI_Recv(&number,1,MPI_INT,id2,99,grid_comm,&status);
-    	printf("I received %d\n",number);
-    }
-     if (my_rank == id2)
-    {
-    	int Data;
-    	MPI_Recv(&Data,1,MPI_INT,id1,99,cl,&status);
-    	printf("I received %d\n",Data);
-    	Data = Data + 1;
-    	MPI_Send(&Data, 1, MPI_INT, id1,99, grid_comm);
-    }
-    */
+	MPI_Barrier(MPI_COMM_WORLD);
+
+  	if (gridRank == 0)
+  	{
+  		/*
+  		for (int i = 0; i < Dims[0]; ++i)
+  		{
+  			for (int j = 0; j < Dims[1]; ++j)
+  			{
+  				int id;
+  				int coord[] = {i,j};
+  				MPI_Cart_rank(grid_comm, coord, &id);
+  				printf("| %d",id );
+  			}
+  			printf("\n");
+  		}
+  		*/
+  	}
+  	MPI_Barrier(MPI_COMM_WORLD);
+
+  	// Communication via les lignes, on broadcast la diagonal
+
+  	int A = -1;
+
+  	if (gridRank==0)
+		{
+			A = 1;
+			printf("Init value of A for rank %d = %d \n", gridRank, A);
+		}
+	MPI_Barrier(MPI_COMM_WORLD);
+  	if (gridRank==4) 
+		{
+			A = 2;
+			printf("Init value of A for rank %d = %d \n", gridRank, A);
+		}
+	MPI_Barrier(MPI_COMM_WORLD);
+	if (gridRank==8) 
+		{
+			A = 3;
+			printf("Init value of A for rank %d = %d \n", gridRank, A);
+		}
+	MPI_Barrier(MPI_COMM_WORLD);
+  	printf("Row: %d / my_rank : %d / lrank : %d / rrank : %d \n", row, gridRank, lRank, rRank);
+  	MPI_Bcast(&A,1,MPI_INT,rRank,line_comm);
+  	
+  	
+  	
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	printf("Rank %d value of %d from process %d\n", gridRank, A, ROOT);
+
+
+	  	//printf("Rank %d received %d\n", my_rank, A);
+		
+
+
+
 
 	MPI_Finalize(); // cloturer l'environement MPI
 	
